e82d65fb544eb96a7e66cab2eb3cd046a2b5a79f
TORR3S <fjtnavarro@gmail.com>
2020-01-02 19:20:13 +0100

    Update checkNIFv2.php

--- checkNIFv2.php
+++ checkNIFv2.php
@@ -1,6 +1,6 @@
 <?php
-/* http://es.wikipedia.org/wiki/N%C3%BAmero_de_identificaci%C3%B3n_fiscal
- * http://es.wikipedia.org/wiki/C%C3%B3digo_de_identificaci%C3%B3n_fiscal
+/* https://es.wikipedia.org/wiki/N%C3%BAmero_de_identificaci%C3%B3n_fiscal
+ * https://es.wikipedia.org/wiki/C%C3%B3digo_de_identificaci%C3%B3n_fiscal
  */
 $NIF_Type = [
     'A'=>'Sociedad Anónima',
