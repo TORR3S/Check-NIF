e9c986799d2a7cfe07b6b32da752294173aa96fe
TORR3S <fjtnavarro@gmail.com>
2020-01-02 19:21:40 +0100

    Update checkNIF.js

--- checkNIF.js
+++ checkNIF.js
@@ -1,6 +1,5 @@
-/*
- * http://es.wikipedia.org/wiki/N%C3%BAmero_de_identificaci%C3%B3n_fiscal
- * http://es.wikipedia.org/wiki/C%C3%B3digo_de_identificaci%C3%B3n_fiscal
+/* https://es.wikipedia.org/wiki/N%C3%BAmero_de_identificaci%C3%B3n_fiscal
+ * https://es.wikipedia.org/wiki/C%C3%B3digo_de_identificaci%C3%B3n_fiscal
  */
 function checkNIF(nif) {
     nif = nif.toUpperCase().replace(/[\s\-]+/g, '');
